2bce956b7a86eeef55812d2eb5032cd2dfb031ba
Italo Flauber Cavalcanti de Santana <italo.santana@xpi.com.br>
2020-07-04 16:10:03 -0300

    first commit

---
+++ RedisPubTest/Program.cs
@@ -0,0 +1,19 @@
+﻿using StackExchange.Redis;
+using System;
+
+namespace RedisPubTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var redis = RedisStore.RedisCache;
+
+            var quote = redis.StringGet("_mktDataCurrentQuotePETR4");
+
+            ISubscriber pub = redis.Multiplexer.GetSubscriber();
+
+            pub.Publish("test124", quote);
+        }
+    }
+}
